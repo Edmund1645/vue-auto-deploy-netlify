d8f0e03093ed22f8f13a7ca15af8270bfe72f78c
edmund1645 <edmund.timfon@gmail.com>
2020-09-10 23:10:10 +0100

    setup tests

---
+++ __tests__/App.spec.js
@@ -0,0 +1,37 @@
+import { mount } from "@vue/test-utils";
+import App from "@/App.vue";
+
+describe("App.vue", () => {
+  it("should add a task when button is clicked", async () => {
+    const wrapper = mount(App);
+    const task = "Do laundry";
+    wrapper.find("input.task-input").setValue(task);
+    await wrapper.find("button.add-task-button").trigger("click");
+    expect(wrapper.vm.$data.todos.indexOf(task)).toBeGreaterThan(-1);
+  });
+
+  it("should render the tasks", () => {
+    const todos = ["pick up groceries", "buy guitar pick"];
+    const wrapper = mount(App, {
+      data() {
+        return {
+          todos
+        };
+      }
+    });
+    expect(wrapper.find("div#todos>ul").text()).toContain(todos[0]);
+  });
+
+  it("should delete a task when the delete button is clicked", async () => {
+    const todos = ["Order pizza for dinner"];
+    const wrapper = mount(App, {
+      data() {
+        return {
+          todos
+        };
+      }
+    });
+    await wrapper.find("button.delete-task-button").trigger("click");
+    expect(wrapper.vm.$data.todos.indexOf(todos[0])).toEqual(-1);
+  });
+});
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Todo app</h1>
+    <div id="add-todos">
+      <input v-model="task" type="text" class="task-input" />
+      <button class="add-task-button" @click="addTask">Add Task</button>
+    </div>
+    <div id="todos">
+      <ul>
+        <li v-for="(todo, index) in todos" :key="index" class="todo-item">
+          <span>{{ todo }}</span>
+          <button class="delete-task-button" @click="deleteTask(todo)">Delete</button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      task: "",
+      todos: []
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.task !== "") {
+        this.todos.push(this.task);
+        this.task = "";
+      }
+    },
+    deleteTask(todoItem) {
+      this.todos = this.todos.filter(item => item !== todoItem);
+    }
   }
 };
 </script>
@@ -24,5 +48,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+  margin: auto;
+}
+.task-input {
+  margin-right: 10px;
+}
+.todo-item {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 20px;
+  border: 1px solid #b9b9b9;
+  margin-top: 4px;
 }
 </style>
